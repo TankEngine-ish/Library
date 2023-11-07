c37f93d5272a4561b580407eff189ee894d74061
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2023-11-07 15:46:43 +0200

    [chore] finish functionality

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 
 const formContainer = document.querySelector("#container");
 const form = document.querySelector("#form");
-const newBook = document.querySelector("#add-book");
+const newBook = document.querySelector("#new-book");
 const overlay = document.querySelector(".overlay");
 const closeButton = document.querySelector(".close");
 const bookshelf = document.querySelector(".bookshelf");
 let books = JSON.parse(localStorage.getItem("books")) || [];
-
 let formOpen = false;
 
 class Book {
